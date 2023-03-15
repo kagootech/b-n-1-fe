717323269b1417ce6c4d240c08a24aadaeb649ec
zacky293 <gaming29zacky@gmail.com>
2023-03-15 14:11:37 +0700

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,19 @@ import Checkout from "./Pages/Product/Checkout";
 import Profile from "./Pages/Profile/Profile";
 import Login from "./Pages/Auth/Login";
 
+import { Product } from "./Pages/Dashboard/Product/Product";
+import { MyProductView } from "./Pages/Dashboard/Product/View";
+import { EditProduct } from "./Pages/Dashboard/Product/Edit";
+import { PromotionProduct } from "./Pages/Dashboard/Promotion/Promotion";
+import { UpdatePromotion } from "./Pages/Dashboard/Promotion/Update";
+import { LifecycleProduct } from "./Pages/Dashboard/Lifecycle/Lifecycle";
+import { AddNewProduct } from "./Pages/Dashboard/Product/AddNew";
+import { ProductAll } from "./Pages/Product/All";
+import { AddNewProductComponent } from "./Component/Dashboard/Product/MyProduct/NewProduct.component";
+import { Wishlist } from "./Pages/Dashboard/Wishlist/Wishlist";
+import { SearchPage } from "./Pages/Search";
+import { AddNewVariant } from "./Pages/Dashboard/Product/AddNewVariant";
+
 function AuthRoutes() {
   return (
     <>
@@ -59,23 +72,6 @@ function App() {
         <Routes>
           <Route index={true} path="/" element={<Home logged={logged} />} />
           <Route path="/product/:state" element={<View />} />
-          <Route path="/product/:state/checkout" element={<Checkout />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/search/name" element={<ProductAll />} />
-          <Route path="/user/product" element={<Product />} />
-          <Route path="/user/new/product" element={<AddNewProduct />} />
-          <Route path="/user/new/product/variant" element={<AddNewVariant />} />
-          <Route path="/user/product/:state" element={<MyProductView />} />
-          <Route path="/user/add/product/" element={<AddNewProduct />} />
-          <Route path="/user/edit/product/:state" element={<EditProduct />} />
-          <Route path="/user/promotion" element={<PromotionProduct />} />
-          <Route
-            path="/user/update/product/:state"
-            element={<UpdatePromotion />}
-          />
-          <Route path="/user/status/product" element={<LifecycleProduct />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/login/redirect" element={<Login />} />
         </Routes>
         {logged ? <AuthRoutes /> : <GuestRoutes setLogged={setLogged} />}
         {/* <a href="https://saweria.co" className="fixed bottom-3 right-4 bg-yellow-400 px-5 py-2 rounded-full shdadow">
